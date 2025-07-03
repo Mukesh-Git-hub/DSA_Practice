8984c871dc80b659bda678ab28d659237e5ab9bb
Mukesh-Git-hub <mukeshgvij2003@gmail.com>
2025-07-03 13:37:48 +0530

    add bubblesort

---
+++ Arraydata.java
@@ -0,0 +1,7 @@
+public class Arraydata {
+    public static void main(String[] args) {
+        int []array={50,40,30,20,10};
+       Bubblesort sort=new Bubblesort();
+       sort.callsort(array);
+    }
+}
---
+++ Bubblesort.java
@@ -0,0 +1,21 @@
+public class Bubblesort {
+    void callsort(int[] array){
+        int n= array.length;;
+        for(int i=0;i<=n-2;i++){
+            for(int j=1;j<=n-1;j++){
+                if(array[j-1]>array[j]){
+                    swap(array,j-1,j);
+                }
+            }
+        }
+        for(int each:array){
+            System.out.println(each+" ");
+        }
+    }
+    static void swap(int []array,int a,int b){
+        int temp=array[a];
+        array[a]=array[b];
+        array[b]=temp;
+    }
+
+}
