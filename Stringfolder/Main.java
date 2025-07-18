e567a026acb4fc29bb4c4c736f7d0625ec27c629
Mukesh-Git-hub <mukeshgvij2003@gmail.com>
2025-07-18 23:30:34 +0530

    add one in string folder

---
+++ Stringfolder/Main.java
@@ -0,0 +1,9 @@
+package Stringfolder;
+
+public class Main {
+    public static void main(String[] args) {
+        int array[]={1,2,3};
+        Plusone p = new Plusone();
+        p.call(array);
+    }
+}
