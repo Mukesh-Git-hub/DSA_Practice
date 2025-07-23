1fd6245ad1737746ad0a12157c20f14a3636f753
Mukesh-Git-hub <mukeshgvij2003@gmail.com>
2025-07-23 23:04:50 +0530

    add another method in same question

--- ArrayFolder/BinarySearch/serachRotated.java
+++ ArrayFolder/BinarySearch/serachRotated.java
@@ -2,28 +2,54 @@ package ArrayFolder.BinarySearch;
 
 public class serachRotated {
     public int search(int[] nums, int target) {
-        int low=0;
-        int high=nums.length-1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid] ==target) return mid;
-            if(nums[low]<= nums[mid]){
-                if(nums[low]<=target && target<=nums[mid]){
-                    high=mid-1;
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
                 }
-                else{
-                    low=mid+1;
-                }
-            }
-            else{
-                if(nums[mid]<=target && target<=nums[high]){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
                 }
             }
         }
         return -1;
+    }
+
+    public boolean search2(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return true;
+            if (nums[low] == nums[mid] && nums[high] == nums[mid]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+
     }
 }
