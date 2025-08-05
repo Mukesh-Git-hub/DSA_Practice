90747738d5328969ae6bc59fa90a97497b1f47b0
Mukesh-Git-hub <mukeshgvij2003@gmail.com>
2025-08-05 23:08:30 +0530

    sliding window

---
+++ SlidingWindow/Lengthofsubstringnorepeat.java
@@ -0,0 +1,30 @@
+package SlidingWindow;
+
+import java.util.HashSet;
+
+public class Lengthofsubstringnorepeat {
+    void method(){
+        String s="abcabcbb";
+        int left=0;
+        int right=0;
+        int n=s.length();
+        HashSet<Character> set = new HashSet<>();
+
+
+        int maxlength=0;
+        while(right<n){
+            char current = s.charAt(right);
+            while (set.contains(current)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(current);
+            maxlength = Math.max(maxlength, right - left + 1);
+            right++;
+        }
+        System.out.println("maxlength"+maxlength);
+    }
+
+
+
+}
